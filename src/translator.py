1cf023320ac213e11ffc96873aa1846509c2d9c8
izaq09 <48687476+izaq09@users.noreply.github.com>
2025-07-13 15:53:31 +0800

    update (translator): Move prompt content to data folder, Enable dynamic mapping

--- src/translator.py
+++ src/translator.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 from google import genai
 from google.genai import types
@@ -18,67 +19,9 @@ Translate the following TCL code snippets into clean, idiomatic SKILL code suita
 
 Please use the following conceptual mappings when translating:
 
-1.  **Variables:**
-    * TCL: `set variable_name value`  -> SKILL: `variable_name = value`
-    * TCL: `$variable_name` -> SKILL: `variableName` (SKILL does not use '$' for variable access)
+{syntax_reference}
 
-2.  **Standard Output:**
-    * TCL: `puts "string"` -> SKILL: `printf("string\n")`
-
-3.  **Procedures/Functions:**
-    * TCL: `proc name {{args}} {{ ... }}` -> SKILL: `procedure(name(args) ... )`
-
-4. **Localize variables in Procedures:**
-    * SKILL uses `prog` to define local variables within procedures.
-    # `procedure(procName(args) prog((local_variable) ... );prog);procedure`
-
-5.  **Loops:**
-    * TCL: `for {{init}} {{end_condition}} {{increment}} {{ ... }}` -> SKILL: `for(var_name start_value end_value ... )` (For loop in SKILL defines start and end values explicitly)
-
-6.  **Mathematical Operations:**
-    * TCL: `[expr $x + $y]` -> SKILL: `x + y`
-    * TCL: `[expr $x * $y]` -> SKILL: `x * y`
-
-7.  **Control Flow:**
-    * TCL: `if {{condition}} {{ ... }} else {{ ... }}` -> SKILL: `if(condition then ... else ...)`
-
-### Example 1
-TCL Input:
-```tcl
-proc procName {{arg1 arg2}} {{
-    puts "Hello, World!"
-}}
-```
-
-SKILL Output:
-```skill
-procedure(procName(arg1 arg2)
-    prog(()
-        printf("%s %s\\n" arg1 arg2)
-    );prog
-);procedure
-```
-
-### Example 2
-TCL Input:
-```tcl
-proc procName {{arg1 arg2}} {{
-    for {{set i 0}} {{$i <= 10}} {{incr i}} {{
-        puts "Iteration $i: $arg1 $arg2"
-    }}
-}}
-```
-
-SKILL Output:
-```skill
-procedure(procName(arg1 arg2)
-    prog(()
-        for(i 0 10
-            printf("Iteration %d: %s %s\n" i arg1 arg2)
-        );for
-    );prog
-);procedure
-```
+{translation_example}
 
 ### Target Translation
 TCL Input:
@@ -90,10 +33,52 @@ TCL Input:
 
 
 def parse_args():
+    """
+    Parse command line arguments for the script.
+    """
     parser = argparse.ArgumentParser(description="Translate TCL file to SKILL file")
     parser.add_argument("--tcl_file", type=str, help="Path to the input TCL file")
     parser.add_argument("--skill_file", type=str, help="Path to the output SKILL file")
     return parser.parse_args()
+
+
+def get_syntax_reference(tcl_code: str) -> str:
+    """
+    Load the syntax reference from a file.
+
+    Returns:
+        str: The syntax reference content.
+    """
+
+    # Get the general syntax reference from a file
+    with open("data/syntax_reference_general.txt", "r") as f:
+        reference = f.read()
+
+    # Get the specific syntax reference for the Standard Cell Placement Optimizer API
+    with open("data/syntax_reference_api.json", "r") as f:
+        api_reference = json.load(f)
+
+    api_reference_list = []
+    for key, value in api_reference.items():
+        if key in tcl_code:
+            api_reference_list += value
+
+    if api_reference_list:
+        reference += "\n**Standard Cell Placement Optimizer API**\n"
+        reference += "\n".join(api_reference_list)
+
+    return reference
+
+
+def get_translation_example() -> str:
+    """
+    Load the translation example from a file.
+
+    Returns:
+        str: The translation example content.
+    """
+    with open("data/translation_example.txt", "r") as f:
+        return f.read()
 
 
 def generate_prompt(tcl_code: str) -> str:
@@ -106,7 +91,12 @@ def generate_prompt(tcl_code: str) -> str:
     Returns:
         str: The generated prompt.
     """
-    return ChatPromptTemplate.from_template(PROMPT_TEMPLATE).format(tcl_code=tcl_code)
+
+    return ChatPromptTemplate.from_template(PROMPT_TEMPLATE).format(
+        syntax_reference=get_syntax_reference(tcl_code=tcl_code),
+        translation_example=get_translation_example(),
+        tcl_code=tcl_code,
+    )
 
 
 def inference(prompt: str) -> str | None:
@@ -119,14 +109,14 @@ def inference(prompt: str) -> str | None:
     Returns:
         str: The translated SKILL code.
     """
-    logger.info("Starting inference for code generation...")
+    logger.info("Starting inference for code translation...")
 
     client = genai.Client()
 
     response = client.models.generate_content(
         model="gemini-2.5-flash-preview-05-20",
         config=types.GenerateContentConfig(
-            system_instruction="You are an expert EDA (Electronic Design Automation) engineer with extensive knowledge of both TCL and Cadence SKILL programming. Your task is to translate TCL scripts into functionally equivalent SKILL code. "
+            system_instruction="You are an expert EDA (Electronic Design Automation) engineer with extensive knowledge of both TCL and Cadence SKILL programming. Your task is to translate TCL scripts into functionally equivalent SKILL code without any markdown format"
         ),
         contents=prompt,
     )
@@ -134,7 +124,31 @@ def inference(prompt: str) -> str | None:
     return response.text if response else None
 
 
-def translate_tcl_to_skill(tcl_file: str, skill_file: str):
+def translate_tcl_code_to_skill_code(tcl_code: str) -> str | None:
+    """
+    Translates a given TCL code snippet to SKILL code.
+
+    Args:
+        tcl_code (str): The TCL code to be translated.
+
+    Returns:
+        str: The translated SKILL code.
+    """
+    logger.info("Translating TCL code to SKILL code...")
+
+    prompt = generate_prompt(tcl_code)
+
+    response_text = inference(prompt)
+
+    if response_text:
+        logger.info("Translation successful.")
+        return response_text
+    else:
+        logger.error("Failed to generate SKILL code from TCL input.")
+        return None
+
+
+def translate_tcl_file_to_skill_file(tcl_file: str, skill_file: str):
     """
     Translates a TCL file to a SKILL file.
 
@@ -142,22 +156,18 @@ def translate_tcl_to_skill(tcl_file: str, skill_file: str):
         tcl_file (str): Path to the input TCL file.
         skill_file (str): Path to the output SKILL file.
     """
-    logger.info("Translating TCL to SKILL...")
-
     with open(tcl_file, "r") as infile, open(skill_file, "w") as outfile:
         content = infile.read()
 
-        prompt = generate_prompt(content)
+        response_text = translate_tcl_code_to_skill_code(tcl_code=content)
 
-        response = inference(prompt)
-
-        if response:
-            logger.info("Translation successful. Writing to output file...")
-            outfile.write(response)
+        if response_text:
+            logger.info("Writing to output file...")
+            outfile.write(response_text)
         else:
             logger.error("Failed to generate SKILL code from TCL input.")
 
 
 if __name__ == "__main__":
     args = parse_args()
-    translate_tcl_to_skill(args.tcl_file, args.skill_file)
+    translate_tcl_file_to_skill_file(args.tcl_file, args.skill_file)
